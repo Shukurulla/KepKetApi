490cfb04ce703c053e3848dca8367dc101ac6319
Shukurulla <shukurullatursinbayev978@gmail.com>
2024-09-07 15:50:46 +0500

    Start

--- src/routes/notification.routes.js
+++ src/routes/notification.routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/", authMiddleware, notificationRoute.createaNotification);
 router.put("/complate/:id", authMiddleware, notificationRoute.complate);
 router.get("/all/:id", authMiddleware, notificationRoute.getAllNotification);
-router.get("/:id", notificationRoute.getNotification);
+router.get("/:id", notificationRoute.getMyNotification);
 router.put("/:id", authMiddleware, notificationRoute.editNotification);
 router.delete("/:id", authMiddleware, notificationRoute.deleteNotification);
 
