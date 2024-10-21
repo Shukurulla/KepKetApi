4baf36fe65c5ef8249305baa9b2a2d337916a49c
Shukurulla <shukurullatursinbayev978@gmail.com>
2024-10-21 12:37:02 +0500

    Start

--- server.js
+++ server.js
@@ -7,15 +7,14 @@ const { Server } = require("socket.io");
 const admin = require("firebase-admin");
 const notificationModel = require("./src/models/notification.model.js");
 const orderModel = require("./src/models/order.model.js");
-const waiterModel = require("./src/models/waiter.model.js"); // waiterModel importini qo'shish
+const waiterModel = require("./src/models/waiter.model.js");
 const serviceAccountKey = require("./serviceAccountKey.json");
 const routes = require("./src/routes");
-const serverless = require("serverless-http"); // Serverless funksionallik uchun qo'shildi
 
 // Firebase service account kalitini ko'rsatish
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKey),
-  databaseURL: process.env.FIREBASE_DATABASE_URL, // Firebase Realtime Database URL
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
 const app = express();
@@ -40,9 +39,9 @@ const server = http.createServer(app);
 // Socket.io ni o'rnatish
 const io = new Server(server, {
   cors: {
-    origin: "*", // Har qanday manzilni qabul qilish
-    optionsSuccessStatus: 200, // 200 statusini qaytarish
-    credentials: true, // Asosiy ma'lumotlar bilan kelish
+    origin: "*",
+    optionsSuccessStatus: 200,
+    credentials: true,
   },
 });
 
@@ -50,10 +49,9 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log("A user connected");
 
-  // Waiter ulanganda uning ID sini saqlab qo'yish
   socket.on("waiter_connected", (waiterId) => {
     console.log(`Ofitsiant ulandi: ${waiterId} | Socket ID: ${socket.id}`);
-    socket.join(waiterId); // Ofitsiantni uning ID si bo'yicha xonaga qo'shish
+    socket.join(waiterId);
   });
 
   socket.on("send_notification", async (schema) => {
@@ -61,48 +59,39 @@ io.on("connection", (socket) => {
       const { orderId, meals } = schema;
       io.to(schema.waiter.id).emit("get_notification", schema);
 
-      // Buyurtma topish
       const findOrder = await orderModel.findById(orderId);
       if (!findOrder) {
-        return; // Agar buyurtma topilmasa, xatolikni qaytarish
+        return;
       }
 
-      // Bildirishnoma yaratish
       const notification = await notificationModel.create(schema);
-
-      // Buyurtmani yangilash
       await orderModel.findByIdAndUpdate(orderId, {
         prepared: findOrder.prepared.concat(meals),
       });
 
       if (notification) {
-        // Ofitsiantni yangilash
         await waiterModel.findByIdAndUpdate(
           notification.waiter.id,
           { $set: { busy: true } },
           { new: true }
         );
-
-        // Bildirishnomani waiter ga yuborish
       }
     } catch (error) {
-      console.error(error); // Xatolikni konsolga chiqarish
+      console.error(error);
     }
   });
 
   socket.on("chef_connected", (chefId) => {
     console.log(`Chef ulandi: ${chefId} | Socket ID: ${socket.id}`);
-    socket.join(chefId); // Ofitsiantni uning ID si bo'yicha xonaga qo'shish
+    socket.join(chefId);
   });
 });
 
-// **Serverless funktsiyaga aylantirish**
-module.exports.handler = serverless(app, {
-  // Socket.io ni serverless muhitda ishlatish uchun server yaratish
-  binary: ["*/*"],
-  cors: {
-    origin: "*", // Har qanday manzilni qabul qilish
-    methods: ["GET", "POST"], // Qabul qilinadigan metodlar
-    allowedHeaders: ["Content-Type", "Authorization"], // Qabul qilinadigan headerlar
-  },
+// Serverni ishga tushirish
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server ${PORT} portda ishga tushdi`);
 });
+
+// **Export**
+module.exports = { server, app };
