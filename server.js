dcdc40bec5063e6019b7b7413e9782747a2d466e
Shukurulla <shukurullatursinbayev978@gmail.com>
2024-11-01 11:53:58 +0500

    Start

--- server.js
+++ server.js
@@ -21,7 +21,13 @@ const orderModel = require("./src/models/order.model.js");
 
 // CORS sozlamalari
 const corsOptions = {
-  origin: "*",
+  origin: [
+    "http://localhost:5174/",
+    "http://localhost:5173/",
+    "http://localhost:5175/",
+    "https://kep-ket-admin.vercel.app/",
+    "https://unify-liard.vercel.app/",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
